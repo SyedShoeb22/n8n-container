a6eeffb81464fba6628bb4be11fa4ad060162452
Syed Shoeb Abrar <99830705+SyedShoeb22@users.noreply.github.com>
2025-08-18 10:28:15 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "project_id" {}
 variable "region" { default = "us-central1" }
-variable "zone"   { default = "us-central1-a" }
-variable "credentials_file" {}
+variable "zone" { default = "us-central1-a" }
+variable "credentials_file" { default = "gcp-key.json" }
+variable "machine_type" { default = "e2-micro" }
